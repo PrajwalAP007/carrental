dd32bfd3168a3eb6d142fe13b9f04f1a98a0c253
PrajwalAP007 <patilprajwal264@gmail.com>
2023-03-31 13:19:43 +0530

    intail commit

--- server.js
+++ server.js
@@ -118,6 +118,43 @@ app.post('/bookings', async (req, res) => {
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Define the schema for car bookings
+// const buySchema = new mongoose.Schema({
+//   section: String,
+//   carname: String,
+//   carmodel: String,
+//   email: String,
+//   userId: Number,
+//   price: Number,
+//   IdproofNo: Number,
+//   Customer_Name: String,
+//   phoneno: Number,
+//   Customer_address: String,
+//   dropaddress: String,
+// });
+
+
+// const Buynow = mongoose.model('Buynow', buySchema);
+
+
+// app.post('/buynow', async (req, res) => {
+//   try {
+
+//     const buynows = new Buynow(req.body);
+//     await buynows.save();
+//     const email = buynows.email;
+//     client.sendMail({
+//       from: "carlelo264@gmail.com",
+//       to: email,
+//       subject: "Your car booking details",
+//       text: `Hello ${buynows.Customer_Name} Your Car is booked. Details are ${buynows.carname}  ${buynows.carmodel} & it's price is ${buynows.price} your delivery address is  ${buynows.dropaddress} `
+//     });
+//     res.status(201).json({ message: 'Booking created successfully' });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({ message: 'Failed to create booking' });
+//   }
+// });
+
 const buySchema = new mongoose.Schema({
   section: String,
   carname: String,
@@ -132,21 +169,30 @@ const buySchema = new mongoose.Schema({
   dropaddress: String,
 });
 
-
 const Buynow = mongoose.model('Buynow', buySchema);
-
 
 app.post('/buynow', async (req, res) => {
   try {
-
-    const buynows = new Buynow(req.body);
-    await buynows.save();
-    const email = buynows.email;
+    const buynow = new Buynow({
+      section: req.body.section,
+      carname: req.body.carname,
+      carmodel: req.body.carmodel,
+      email: req.body.email,
+      userId: req.body.userId,
+      price: req.body.price,
+      IdproofNo: req.body.IdproofNo,
+      Customer_Name: req.body.Customer_Name,
+      phoneno: req.body.phoneno,
+      Customer_address: req.body.Customer_address,
+      dropaddress: req.body.dropaddress,
+    });
+    await buynow.save();
+    const email = buynow.email;
     client.sendMail({
       from: "carlelo264@gmail.com",
       to: email,
       subject: "Your car booking details",
-      text: `Hello ${buynows.Customer_Name} Your Car is booked. Details are ${buynows.carname}  ${buynows.carmodel} & it's price is ${buynows.price} your delivery address is  ${buynows.dropaddress} `
+      text: `Hello ${buynow.Customer_Name}, your car is booked. Details are ${buynow.carname} ${buynow.carmodel} and its price is ${buynow.price}. Your delivery address is ${buynow.dropaddress}.`
     });
     res.status(201).json({ message: 'Booking created successfully' });
   } catch (err) {
@@ -154,7 +200,6 @@ app.post('/buynow', async (req, res) => {
     res.status(500).json({ message: 'Failed to create booking' });
   }
 });
-
 
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
